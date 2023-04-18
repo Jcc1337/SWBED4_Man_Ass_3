ef2ff984f4a7d6831de77c7fcc1d83ac1b1e9bbb
Jcc1337 <123932310+Jcc1337@users.noreply.github.com>
2023-04-18 14:17:02 +0200

    Authorization for Reception2 Added

--- SW4BED_Man_Ass_3/Pages/Reception2.cshtml.cs
+++ SW4BED_Man_Ass_3/Pages/Reception2.cshtml.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SW4BED_Man_Ass_3.Pages
 {
+    [Authorize("Reception")]
     public class Reception2Model : PageModel
     {
         public void OnGet()
