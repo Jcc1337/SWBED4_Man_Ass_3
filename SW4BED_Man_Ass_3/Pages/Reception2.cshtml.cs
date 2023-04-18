0ca109d0f6db567dd9a503172784a0ddfeec564e
Jcc1337 <123932310+Jcc1337@users.noreply.github.com>
2023-04-18 15:00:47 +0200

    .

--- SW4BED_Man_Ass_3/Pages/Reception2.cshtml.cs
+++ SW4BED_Man_Ass_3/Pages/Reception2.cshtml.cs
@@ -1,13 +1,18 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SW4BED_Man_Ass_3.Data;
 
 namespace SW4BED_Man_Ass_3.Pages
 {
     [Authorize("Reception")]
     public class Reception2Model : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+        public string DateNow { get; set; }
+        //public DisplayModel model { get; set; }
+
+        public async Task OnGetAsync()
         {
         }
     }
