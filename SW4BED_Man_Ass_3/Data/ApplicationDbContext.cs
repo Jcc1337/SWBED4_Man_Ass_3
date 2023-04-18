84e8e50f60d3847847be316a27d6f0ec4ddf09a0
Jcc1337 <123932310+Jcc1337@users.noreply.github.com>
2023-04-18 09:21:15 +0200

    Solution made

---
+++ SW4BED_Man_Ass_3/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SW4BED_Man_Ass_3.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
