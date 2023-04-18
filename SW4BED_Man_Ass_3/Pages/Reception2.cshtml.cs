cf690ae78d61a9765dcecc7ea3ee96d8bd2dcc09
Jcc1337 <123932310+Jcc1337@users.noreply.github.com>
2023-04-18 15:35:40 +0200

    .

--- SW4BED_Man_Ass_3/Pages/Reception2.cshtml.cs
+++ SW4BED_Man_Ass_3/Pages/Reception2.cshtml.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using SW4BED_Man_Ass_3.Data;
+using SW4BED_Man_Ass_3.Models;
 
 namespace SW4BED_Man_Ass_3.Pages
 {
@@ -9,6 +11,7 @@ namespace SW4BED_Man_Ass_3.Pages
     public class Reception2Model : PageModel
     {
         private readonly ApplicationDbContext _context;
+        public List<GuestCheckIn> GuestCheckedIn { get; set; } = new List<GuestCheckIn>();
         public string DateNow { get; set; }
         public ListDisplay listDisplay { get; set; }
 
@@ -19,8 +22,24 @@ namespace SW4BED_Man_Ass_3.Pages
             public int Children { get; set; } = 0;
         }
 
+        public Reception2Model(ApplicationDbContext context)
+        {
+            _context=context;
+            listDisplay=new ListDisplay();
+
+            //Udfilterer timer, sekunder osv.
+            DateNow = DateTime.Now.Day + "/" + DateTime.Now.Month;
+        }
+
         public async Task OnGetAsync()
         {
+            var dbGuestCheckIns = await _context.GuestCheckIns
+                .Where(b => b.Date.Day == DateTime.Now.Day && b.Date.Month == DateTime.Now.Month)
+                .ToListAsync();
+
+            if (dbGuestCheckIns == null) { RedirectToPage("Error"); return; }
+
+            GuestCheckedIn = dbGuestCheckIns;
         }
     }
 }
