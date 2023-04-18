acb6e0795957ab0c3c1a434282138d6f7442656d
Madsthomsen123 <104567673+Madsthomsen123@users.noreply.github.com>
2023-04-18 10:00:10 +0200

    Login and logout functionality

---
+++ SW4BED_Man_Ass_3/Data/Migrations/20230418075719_logins.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SW4BED_Man_Ass_3.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class logins : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
