e8d62c4a8d4fff771182be6a35c29a88f68e0a7c
Madsthomsen123 <104567673+Madsthomsen123@users.noreply.github.com>
2023-04-19 13:45:37 +0200

    nu med hub

---
+++ SW4BED_Man_Ass_3/hub/HubKitchen.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SW4BED_Man_Ass_3.hub
+{
+    public class HubKitchen : Hub
+    {
+        public async Task kitchenReaload() 
+        {
+            await Clients.All.kitchenReload();
+        }
+    }
+}
